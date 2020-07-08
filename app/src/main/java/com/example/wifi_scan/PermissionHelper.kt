cd18be70bb96dd6f4a2864331984f1aadc26705c
Tarik <tarikbyazici61@gmail.com>
2020-07-08 13:47:11 +0300

    location permission added

---
+++ app/src/main/java/com/example/wifi_scan/PermissionHelper.kt
@@ -0,0 +1,36 @@
+package com.example.wifi_scan
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class PermissionHelper(private val activity: Activity) {
+
+    private val REQUEST_CODE = 101
+
+    fun checkPermission(block: () -> Unit) {
+        val permission = ContextCompat.checkSelfPermission(
+            activity,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(activity, R.string.permission_granted, Toast.LENGTH_SHORT).show()
+            makeRequest()
+        } else {
+            block()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_CODE
+        )
+    }
+
+}
